dbb8b1170fe1a117e15ba4ee2eda402976721e25
Abhay Patil <abhaypatil8625@gmail.com>
2026-09-12 01:00:26 +0530

    Configure secure MySQL connection for production

--- src/config/db.js
+++ src/config/db.js
@@ -1,11 +1,14 @@
-const mysql = require('mysql2/promise');
+const mysql = require("mysql2/promise");
 
 const pool = mysql.createPool({
     host: process.env.DB_HOST,
-    user : process.env.DB_USER,
+    user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    port: process.env.DB_PORT
+    port: process.env.DB_PORT,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 module.exports = pool;
